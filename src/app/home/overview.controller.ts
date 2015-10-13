766cc0ed2047e087231dc613b4ba2a52b0fadfab
Jeppe Stougaard <jstougaard@gmail.com>
2015-10-13 10:49:32 +0200

    Added colors to overview

--- src/app/home/overview.controller.ts
+++ src/app/home/overview.controller.ts
@@ -3,6 +3,17 @@
 
 class OverviewController {
 
+    public colors = {
+        lead1: "#C55A26",
+        lead2: "#066031",
+        lead3: "#007ee5",
+        lead4: "#7d6a96",
+        lead5: "#f9fc00",
+        lead6: "#fbb4e3",
+        lead7: "#92c272",
+        lead8: "#b5022c"
+    };
+
   /* @ngInject */
   constructor(
     private $rootScope: core.IRootScope,
